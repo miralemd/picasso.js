9243f2ed432e680b3c73f6437690336373e2c724
Klavdiia Koval <kkv@qlik.com>
2016-03-07 21:15:23 +0100

    Added some converting functions for RGB and HSL

--- src/colors/hsla-color.js
+++ src/colors/hsla-color.js
@@ -2,6 +2,45 @@ function toPercentage(val) {
 	return val * 100;
 }
 
+/**
+ *
+ * @param h - The hue
+ * @param s - The saturation
+ * @param l - The lightness
+ * @returns {*[]} - r, g, b in set [0,255]
+ */
+function toRGB( h, s, l ) {
+	let r, g, b;
+
+	h = h / 360;
+
+	if( s === 0 ){
+		r = g = b = l;
+	}
+	else {
+		let hue2rgb = ( p, q, t ) => {
+			if ( t < 0 ) { t += 1; }
+			if ( t > 1 ) { t -= 1; }
+			if ( t < 1 / 6 ) { return p + ( q - p ) * 6 * t; }
+			if ( t < 1 / 2 ) { return q; }
+			if ( t < 2 / 3 ) { return p + ( q - p ) * (2 / 3 - t ) * 6; }
+			return p;
+		};
+
+		var q = l < 0.5 ? l * ( 1 + s ) : l + s - l * s;
+		var p = 2 * l - q;
+		r = hue2rgb( p, q, h + 1 / 3 );
+		g = hue2rgb( p, q, h );
+		b = hue2rgb( p, q, h - 1 / 3 );
+	}
+
+	r = Math.round( r * 255 );
+	g = Math.round( g * 255 );
+	b = Math.round( b * 255 );
+
+	return `${r}, ${g}, ${b}`;
+}
+
 export default class HslaColor {
 	constructor( h, s, l, a = 1) {
 		this.h = h;
@@ -12,19 +51,17 @@ export default class HslaColor {
 
 	/**
 	* Returns a hsl string representation of this color using "bi-hexcone" model for lightness
-	* @param {boolean} luma - Whether to use luma calculation
 	* @return {string} In format hsl(0,0,0)
 	*/
-	toHSL( luma ) {
-		return `hsl(${this.h}, ${this.s}%, ${this.l}%)`;
+	toHSL() {
+		return `hsl(${this.h}, ${toPercentage(this.s)}%, ${toPercentage(this.l)}%)`;
 	}
 
 	/**
 	 * Returns a hsla string representation of this color using "bi-hexcone" model for lightness
-	 * @param {boolean} luma - Whether to use luma calculation
 	 * @return {string} In format hsla(0,0,0,0)
 	 */
-	toHSLA( luma ) {
+	toHSLA() {
 		return this.toString();
 	}
 
@@ -33,7 +70,8 @@ export default class HslaColor {
 	 * @return {string} An rgb string representation of this color
 	 */
 	toRGB() {
-
+		let rgb = toRGB( this.h, this.s, this.l );
+		return `rgb(${rgb})`;
 	}
 
 	/**
@@ -41,10 +79,13 @@ export default class HslaColor {
 	 * @return {string} An rgba string representation of this color.
 	 */
 	toRGBA() {
-
+		let rgba = toRGB( this.h, this.s, this.l );
+		return `rgba(${rgba}, ${this.a})`;
 	}
 
 	toString() {
 		return `hsla(${this.h}, ${toPercentage(this.s)}%, ${toPercentage(this.l)}%, ${this.a})`;
 	}
 }
+
+
--- src/colors/rgba-color.js
+++ src/colors/rgba-color.js
@@ -1,3 +1,28 @@
+function toHSL( r, g, b ) {
+	r = r / 255;
+	g = g / 255;
+	b = b / 255;
+
+	let max = Math.max( r, g, b );
+	let min = Math.min( r, g, b );
+	let h, s, l = ( max + min ) / 2;
+
+	if( max === min ){
+		h = s = 0;
+	}else{
+		let d = max - min;
+		s = l > 0.5 ? d / ( 2 - max - min ) : d / ( max + min );
+		switch(max){
+			case r: h = ( g - b ) / d + ( g < b ? 6 : 0 ); break;
+			case g: h = ( b - r ) / d + 2; break;
+			case b: h = ( r - g ) / d + 4; break;
+		}
+		h /= 6;
+	}
+
+	return `${Math.round( h * 360)}, ${Math.round( s * 100)}, ${Math.round( l * 100)}`;
+}
+
 export default class RgbaColor {
     constructor( r, g, b, a = 1 ) {
         this.r = r;
@@ -31,21 +56,21 @@ export default class RgbaColor {
 	}
 
 	/**
-	* Returns a hsl string representation of this color using "bi-hexcone" model for lightness
-	* @param {boolean} luma - Whether to use luma calculation
-	* @return {string} In format hsl(0,0,0)
-	*/
-	toHSL( luma ) {
-
+	 * Returns a hsla string representation of this color using "bi-hexcone" model for lightness
+	 * @return {string} In format hsl(0,0,0)
+	 */
+	toHSL() {
+		let value = toHSL( this.r, this.g, this.b);
+		return `hsl(${value})`;
 	}
 
-    /**
-	 * Returns a hsla string representation of this color using "bi-hexcone" model for lightness
-	 * @param {boolean} luma - Whether to use luma calculation
+	/**
+	 * Returns a hsla string representation of this color
 	 * @return {string} In format hsla(0,0,0,0)
 	 */
-	toHSLA( luma ) {
-
+	toHSLA() {
+		let value = toHSL( this.r, this.g, this.b);
+		return `hsla(${value}, ${this.a})`;
 	}
 
 	/**
@@ -78,7 +103,7 @@ export default class RgbaColor {
      * @param {number} t The interpolation value in the range (0-1).
      * @return {string} The blend as an rgb string.
      */
-    blend() {
+    blend( ) {
 
     }
 
