563702ee0ac2367a726d19494a3da40ea9d33703
Christofer Bengtsson <cbt@qlik.com>
2016-03-10 14:02:07 +0100

    Now handles different color types

--- src/scales/interpolators/color.js
+++ src/scales/interpolators/color.js
@@ -1,10 +1,6 @@
 import {default as color} from "../../colors/color";
-import {default as numeric} from "./numeric";
-
 
 export default {
-	color,
-	numeric,
 	/**
 	 * Interpolate two colors
 	 * @param  {object} from The color to interpolate from
@@ -13,9 +9,6 @@ export default {
 	 * @return {object}      The interpolated color
 	 */
 	interpolate: function( from, to, t ) {
-		let c1 = typeof from === "string" ? this.color( from ) : from,
-			c2 = typeof to === "string" ? this.color( to ) : to;
-		return this.color(`rgba(${Math.round(this.numeric.interpolate( c1.r, c2.r, t ))}, ${Math.round(this.numeric.interpolate( c1.g, c2.g, t ))},	${Math.round(this.numeric.interpolate( c1.b, c2.b, t ))}, ${Math.round(this.numeric.interpolate( c1.a, c2.a, t ))})`
-		);
+		return color.interpolate(from, to, t);
 	}
 };
