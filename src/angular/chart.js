34089bc9297d88daad9dd0f888ba650221e1e032
Miralem Drek <mek@qlik.com>
2016-03-24 12:57:31 +0100

    More componentification
    Remove "to be deprecated" replace property

--- src/angular/chart.js
+++ src/angular/chart.js
@@ -1,5 +1,5 @@
 import pic from "./picasso-angular-core";
-import { addDirective } from "./components";
+import { addDirective, addComponent } from "./components";
 
 addDirective( "picChartContent", function() {
 	return {
@@ -8,7 +8,6 @@ addDirective( "picChartContent", function() {
 			options: "="
 		},
 		restrict: "E",
-		replace: true,
 		template: "",
 		link: function( scope, element ) {
 			element.append( pic.compile( `<pic-${scope.options.type} data='data' options='options'></pic-${scope.options.type}>` )( scope ) );
@@ -16,21 +15,17 @@ addDirective( "picChartContent", function() {
 	};
 } );
 
-addDirective( "picChart", function() {
-	return {
-		scope: {
-			data: "=",
-			options: "="
-		},
-		restrict: "E",
-		replace: true,
-		template: `
-		<div class="pic-chart">
-			<div class="pic-chart__title">Title goes here</div>
-			<div class="pic-chart__subtitle">Subtitle goes here</div>
-			<pic-chart-content class="pic-chart__content" data='data' options='options'></pic-chart-content>
-			<div class="pic-chart__subtitle">Footnote goes here</div>
-		</div>
-		`
-	};
+addComponent( "picChart", {
+	bindings: {
+		data: "=",
+		options: "="
+	},
+	template: `
+	<div class="pic-chart">
+		<div class="pic-chart__title">Title goes here</div>
+		<div class="pic-chart__subtitle">Subtitle goes here</div>
+		<pic-chart-content class="pic-chart__content" data='$ctrl.data' options='$ctrl.options'></pic-chart-content>
+		<div class="pic-chart__subtitle">Footnote goes here</div>
+	</div>
+	`
 } );
