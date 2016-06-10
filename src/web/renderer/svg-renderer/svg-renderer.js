0fc5db3d0c51e75ff008f14c84a1c8f167377ad2
Miralem Drek <mek@qlik.com>
2016-06-10 16:55:31 +0200

    Update jsdoc

--- src/web/renderer/svg-renderer/svg-renderer.js
+++ src/web/renderer/svg-renderer/svg-renderer.js
@@ -2,13 +2,7 @@ import { tree } from "./svg-tree";
 import { svgNs } from "./svg-nodes";
 
 export default class SVGRenderer {
-	/**
-	 * Constructor
-	 * @param  {TreeCreator} treeCreator - Function used to create the DOM tree..
-	 * @param  {SVGCreator} nodeCreator - Function used to create nodes.
-	 * @param  {SVGMaintainer} nodeMaintainer - Function used to update nodes.
-	 * @param  {SVGDestroyer} nodeDestroyer - Function used to destroy nodes.
-	 */
+
 	constructor( treeFn, ns ) {
 		this.ns = ns;
 		this.tree = treeFn();
@@ -75,33 +69,3 @@ export function renderer() {
 	return new SVGRenderer( tree, svgNs );
 }
 
-/**
- * Create an SVGElement and attach to parent.
- * @callback SVGCreator
- * @param {String} type - The type of element to create.
- * @param {SVGElement} parent - The parent element to append the new element to.
- * @returns {SVGElement} The created element
- */
-
-/**
- * Update the element with content from item.
- * @callback SVGMaintainer
- * @param {SVGElement} el - The element to update
- * @param {Object} item - The object to use as input for the update
- */
-
-/**
- * Detach element from its parent.
- * @callback SVGDestroyer
- * @param {SVGElement} el - Element to destroy.
- */
-
-/**
- * Create, update and destroy nodes.
- * @callback TreeCreator
- * @param {Object[]} existing - The existing items in the tree.
- * @param {Object[]} active - The new items to create the tree from.
- * @param {SVGCreator} creator - Function used to create nodes.
- * @param {SVGMaintainer} maintainer - Function used to update nodes.
- * @param {SVGDestroyer} destroyer - Function used to destroy nodes.
- */
