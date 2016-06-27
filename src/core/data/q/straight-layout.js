ecd2ab10ab2befcf60759ce7a72b64448dbbda88
Miralem Drek <mek@qlik.com>
2016-06-27 12:41:41 +0200

    Cleanup data

--- src/core/data/q/straight-layout.js
+++ src/core/data/q/straight-layout.js
@@ -1,3 +1,6 @@
+import { resolve } from "../json-path-resolver";
+import { metaToDataPath } from "./meta-to-data";
+
 function normalizeMeta( obj ) {
 	if ( "qStateCounts" in obj ) { // dimension
 		return {
@@ -13,40 +16,13 @@ function normalizeMeta( obj ) {
 	}
 }
 
-function resolvePath( path, obj ) {
-	let arr = path.replace( /^\//, "" ).split( /\// ),
-		container = obj;
-	for ( let i = 0; i < arr.length; i++ ) {
-		if ( !arr[i] && Array.isArray( container ) ) {
-			return container.map( v => {
-				return resolvePath( arr.slice( i + 1 ).join( "/" ), v );
-			} );
-		} else if ( arr[i] in container ) {
-			container = container[arr[i]];
-		}
-	}
-
-	return container;
-}
-
-function sourceToDataPath( path, meta ) {
-	let p = path,
-		numDimz = meta.qHyperCube.qDimensionInfo.length;
-	p = p.replace( /(qMeasureInfo\/)(\d+)/g, function( match, m, idx ){
-		return ["qDataPages", "${page}", "qMatrix", "", numDimz + +idx].join( "/" );
-	} );
-	p = p.replace( /qDimensionInfo/g, "qDataPages/${page}/qMatrix/" );
-	p = p.replace( /qAttrExprInfo/g, "qAttrExps/qValues" );
-	return p;
-}
-
 export default class StraightLayout {
 	constructor( Promise ) {
 		this.Promise = Promise;
 	}
 
 	metaOf( path ) {
-		return normalizeMeta( resolvePath( path, this._layout ) );
+		return normalizeMeta( resolve( path, this._layout ) );
 	}
 
 	layout( value ) {
@@ -66,8 +42,8 @@ export default class StraightLayout {
 
 	fromSource( source, pageIdx ) {
 		let fn = s => {
-			let path = sourceToDataPath( s, this._layout );
-			return resolvePath( path.replace( "${page}", pageIdx ), this._layout );
+			let path = metaToDataPath( s, this._layout );
+			return resolve( path.replace( "qDataPages/", "qDataPages/" + pageIdx ), this._layout );
 		};
 		return Array.isArray( source ) ? source.map( fn ) : fn( source );
 	}
