2164f7fbb274b12edb752a9366c9f555d327e5d0
Christofer Bengtsson <cbt@qlik.com>
2016-08-29 10:46:21 +0200

    Use ellipsis char

--- src/web/renderer/svg-renderer/svg-text-helpers.js
+++ src/web/renderer/svg-renderer/svg-text-helpers.js
@@ -14,20 +14,27 @@ export default {
 		return rect;
 	},
 
-	getComputedTextLength( text, fontSize, font ) {
-		const struct = { type: "text", text: text, x: 0, y: 0, "font-family": font,	"font-size": fontSize, fill: "white" };
-		return this.getComputedRect( struct ).width;
+	getComputedTextLength( text, fontSize, font, rotation = 0 ) {
+		const struct = { type: "text", text: text, x: 0, y: 0, "font-family": font,	"font-size": fontSize, fill: "white", transform: `rotate(${rotation})` };
+		const tmpDiv = document.createElement( "div" );
+		document.body.insertAdjacentElement( "beforeend", tmpDiv );
+		const rend = renderer();
+		rend.appendTo( tmpDiv );
+		rend.render( [struct] );
+		const rect = tmpDiv.getElementsByTagName( "text" )[0].getComputedTextLength();
+		document.body.removeChild( tmpDiv );
+		return rect;
 	},
 
 	ellipsis( opt ) {
-		opt.reduce = opt.reduce ? opt.reduce : 3; // TODO doesnt make sense with recoursive call where 4 is hard-coded
-		opt.reduceChars = opt.reduceChars ? opt.reduceChars : "...";
+		opt.reduce = opt.reduce ? opt.reduce : 1; // TODO doesnt make sense with recoursive call where 4 is hard-coded
+		opt.reduceChars = opt.reduceChars ? opt.reduceChars : "…";
 
 		let textLength = this.getComputedTextLength( opt.text, opt.fontSize, opt.font );
-		if ( textLength > opt.width && opt.text !== "..." ) {
+		if ( textLength > opt.width && opt.text !== "…" ) {
 			opt.text = opt.text.substr( 0, opt.text.length - opt.reduce );
 			opt.text += opt.reduceChars;
-			opt.reduce = 4;
+			opt.reduce = 2;
 			return this.ellipsis( opt );
 		}
 		return opt.text;
