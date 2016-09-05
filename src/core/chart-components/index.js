63ae565a35e3cbd14f494733003ddc1c66be2059
Christofer Bengtsson <cbt@qlik.com>
2016-09-05 11:10:29 +0200

    Added text component

--- src/core/chart-components/index.js
+++ src/core/chart-components/index.js
@@ -1,11 +1,13 @@
 import { registry } from "../utils/registry";
 import { create } from "./markers/index";
 import { axisFactory } from "./axis/index";
+import { textFactory } from "./text/index";
 
 let reg = registry();
 
 reg.register( "markers", create );
 reg.register( "axes", axisFactory );
+reg.register( "texts", textFactory );
 
 export function components( obj, composer ) {
 	return reg.build( obj, composer );
---
+++ src/core/chart-components/text/index.js
@@ -0,0 +1,19 @@
+import { renderer } from "../../../web/renderer/svg-renderer/svg-renderer";
+import { text } from "./text";
+import { registry } from "../../utils/registry";
+
+let reg = registry();
+reg.register( "text", text );
+
+export function textFactory( texts, composer ) {
+	return texts.map( ( config ) => {
+		const rend = renderer();
+		const element = document.getElementById( config.parent );
+		element.innerHTML = "";
+		rend.rect.width = element.getBoundingClientRect().width;
+		rend.rect.height = element.getBoundingClientRect().height;
+		rend.appendTo( element );
+
+		return reg.registry.text( config, composer, rend );
+	} );
+}
