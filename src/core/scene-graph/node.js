8de7a2dd0e903928047220c8f1216bf237ba061b
Miralem Drek <mek@qlik.com>
2016-09-07 15:47:36 +0200

    Add scene node

---
+++ src/core/scene-graph/node.js
@@ -0,0 +1,134 @@
+export default class Node {
+	constructor() {
+		this._parent = null;
+		this._children = [];
+		this._ancestors = null;
+		this.data = {};
+	}
+
+	/**
+	 * Detaches this node from its parent, if such exists.
+	 * @returns {Node}
+	 */
+	detach() {
+		if ( this._parent ) {
+			this._parent.removeChild( this );
+		}
+		return this;
+	}
+
+	addChild( c ) {
+		if ( !c || !( c instanceof Node ) ) {
+			throw new TypeError( "Expecting a Node as argument, but got " + c );
+		}
+
+		if ( c === this ) {
+			throw "Can not add itself as child!";
+		}
+
+		if ( c._children && c._children.length && this.ancestors.indexOf( c ) >= 0 ) {
+			throw "Can not add an ancestor as child!";
+		}
+
+		if ( c._parent && c._parent !== this ) {
+			c._parent.removeChild( c );
+		}
+
+		//*
+		// really expensive for large arrays
+		let indx = this._children.indexOf( c ); // if child already exists -> remove it, and the push it in last
+		if ( indx >= 0 ) {
+			this._children.splice( indx, 1 );
+		}
+		//*/
+
+		this._children.push( c );
+		c._parent = this;
+		c._ancestors = null;
+
+		return this;
+	}
+
+	addChildren( children ) {
+		let i, num = children ? children.length : 0;
+		for ( i = 0; i < num; i++ ) {
+			this.addChild( children[i] );
+		}
+		return this;
+	}
+
+	/**
+	 * Removes given child node from this node.
+	 * @param {Node} c
+	 * @returns {Node} This object, for chaining purposes.
+	 */
+	removeChild( c ) {
+		let indx = this._children.indexOf( c );
+		if ( indx >= 0 ) {
+			this._children.splice( indx, 1 );
+			c._parent = null;
+			c._ancestors = null;
+		}
+		return this;
+	}
+
+	removeChildren( children ) {
+		let i, num;
+		if ( !this._children ) {
+			return this;
+		}
+		if ( children ) {
+			num = children.length;
+			for ( i = 0; i < num; i++ ) {
+				this.removeChild( children[i] );
+			}
+		}
+		else {
+			while ( this._children.length ) {
+				this.removeChild( this._children[0] );
+			}
+		}
+		return this;
+	}
+
+	/**
+	 * Parent of this node.
+	 * @readonly
+	 * @type {Node}
+	 */
+	get parent() {
+		return this._parent;
+	}
+
+	/**
+	 * Checks whether this node is a branch.
+	 * @readonly
+	 * @type {Boolean} True if this node has children, false otherwise.
+	 */
+	get isBranch () {
+		return this._children && this._children.length;
+	}
+
+	/**
+	 * Children of this node.
+	 * @readonly
+	 * @type {Node[]}
+	 */
+	get children() {
+		return this._children;
+	}
+
+	/**
+	 * Ancestors of this node, including parent.
+	 * @readonly
+	 * @type {Node[]}
+	 */
+	get ancestors () {
+		if ( !this._ancestors ) {
+			let p = this._parent;
+			this._ancestors = p ? [p].concat( p.ancestors ) : [];
+		}
+
+		return this._ancestors;
+	}
+}
