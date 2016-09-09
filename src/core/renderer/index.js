dc287ad5873a5838ebb3770a1f2e55fa9bd2f709
Miralem Drek <mek@qlik.com>
2016-09-09 15:33:42 +0200

    Use registry in renderer

--- src/core/renderer/index.js
+++ src/core/renderer/index.js
@@ -1,31 +1,17 @@
-let prio = ["svg"];
-let renderers = {};
+import { registry } from "../utils/registry";
+let prio = [];
+
+let reg = registry();
 
 export function register( type, fn ) {
-	if ( !type || typeof type !== "string" ) {
-		throw new TypeError( "type must be a string" );
+	if ( reg.add( type, fn ) ) {
+		prio.push( type );
 	}
-	if ( typeof fn !== "function" ) {
-		throw new TypeError( "fn must be a function" );
-	}
-	if ( type in renderers ) {
-		throw new Error( `Renderer of type ${type} already exists` );
-	}
-
-	renderers[type] = fn;
 }
 
-export function create( type ) {
-	if ( typeof type === "undefined" ) {
-		// use prio order order if tech available
-		type = prio[0];
-	}
-	if ( !renderers[type] ) {
+export function renderer( type = prio[0] ) {
+	if ( !reg.has( type ) ) {
 		throw new Error( `Renderer of type ${type} does not exist` );
 	}
-	return renderers[type]();
-}
-
-export function renderer( ...s ) {
-	return create( ...s );
+	return reg.get( type )();
 }
