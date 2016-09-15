325cb8b3616af5dd396cbedb355d2fa5d087b154
Hannes Lindquist <hannes.lindquist@qlik.com>
2016-09-15 10:37:49 +0200

    Create new box prerend with coordinates

--- src/core/chart-components/box/box-prerend.spec.js
+++ src/core/chart-components/box/box-prerend.spec.js
@@ -4,48 +4,36 @@ describe( "boxPrerend", () => {
 
 	let draw = boxPrerend();
 
-	it( "should behave somewhat like an Array", () => {
-		draw.push( "asdf" );
+	it( "should return the correct opposite keys", () => {
 
-		expect( draw ).to.deep.equal( boxPrerend( "asdf" ) );
-		expect( draw.storage[0] ).to.equal( "asdf" );
-		expect( draw.length ).to.equal( 1 );
-	} );
+		// Get the opposite key to what you put in, height when you put in width etc
+		expect( draw.oppositeKey( "width" ) ).to.equal( "height" );
+		expect( draw.oppositeKey( "height" ) ).to.equal( "width" );
 
-	it( "should handle width and height correctly", () => {
-		draw.width = 200;
-		draw.height = 400;
-		draw.flipXY = true;
+		// Same applies to coordinates
+		expect( draw.oppositeKey( "x" ) ).to.equal( "y" );
+		expect( draw.oppositeKey( "y" ) ).to.equal( "x" );
 
-		expect( draw.width ).to.equal( 400 );
-		expect( draw.height ).to.equal( 200 );
+		// With multiple X and Ys
+		expect( draw.oppositeKey( "x1" ) ).to.equal( "y1" );
+		expect( draw.oppositeKey( "y2" ) ).to.equal( "x2" );
 	} );
 
 	it( "should return the correct opposite keys", () => {
-		expect( draw.oppositeKey( "width" ) ).to.equal( "height" );
-		expect( draw.oppositeKey( "height" ) ).to.equal( "width" );
-		expect( draw.oppositeKey( "x1" ) ).to.equal( "y1" );
-		expect( draw.oppositeKey( "y2" ) ).to.equal( "x2" );
-		expect( draw.oppositeKey( "x" ) ).to.equal( "y" );
-		expect( draw.oppositeKey( "y" ) ).to.equal( "x" );
-	} );
+		// Make sure the width and height are set,
+		// all coordinates will be coordinate * width or height
+		draw.width = 2;
+		draw.height = 3;
 
-	it( "should draw flipped objects correctly", () => {
-		draw = boxPrerend();
+		// Check if the coordinates respond correctly to the values
+		expect( draw.coordinateToValue( "width", 2 ) ).to.equal( 4 );
+		expect( draw.coordinateToValue( "height", 3 ) ).to.equal( 9 );
 
-		draw.flipXY = true;
+		expect( draw.coordinateToValue( "x1", 4 ) ).to.equal( 8 );
+		expect( draw.coordinateToValue( "y2", 4 ) ).to.equal( 12 );
 
-		draw.push( {
-			width: 25,
-			height: 50,
-			x1: 20,
-			y2: 40
-		} );
-
-		expect( draw.storage[0].width ).to.equal( 50 );
-		expect( draw.storage[0].height ).to.equal( 25 );
-		expect( draw.storage[0].y1 ).to.equal( 20 );
-		expect( draw.storage[0].x2 ).to.equal( 40 );
+		expect( draw.coordinateToValue( "x", 5 ) ).to.equal( 10 );
+		expect( draw.coordinateToValue( "y", 5 ) ).to.equal( 15 );
 	} );
 
 } );
