cb767de4fde6a11e0819a1ae7b4e12df71968272
Miralem Drek <mek@qlik.com>
2016-09-19 09:35:14 +0200

    General data api

---
+++ src/core/data/data.js
@@ -0,0 +1,33 @@
+/* eslint no-return-assign: 0 */
+export function data() {
+	let dd = {},
+		acc = {
+			rows: () => 0,
+			cols: () => 0,
+			fields: () => []
+		};
+
+	function fn() {}
+
+	fn.data = function( d ) {
+		if ( d ) {
+			dd = d;
+			return fn;
+		}
+		return dd;
+	};
+
+	fn.rows = function( f ) {
+		return f ? ( acc.rows = f, fn ) : acc.rows( dd );
+	};
+
+	fn.cols = function( f ) {
+		return f ? ( acc.cols = f, fn ) : acc.cols( dd );
+	};
+
+	fn.fields = function( f ) {
+		return f ? ( acc.fields = f, fn ) : acc.fields( dd );
+	};
+
+	return fn;
+}
