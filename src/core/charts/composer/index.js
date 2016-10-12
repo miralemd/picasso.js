6b4c7cd189a3b5281b9635889828c5a1cf24b8ba
Miralem Drek <mek@qlik.com>
2016-10-12 11:16:21 +0200

    Re-write composer and scale-factories

---
+++ src/core/charts/composer/index.js
@@ -0,0 +1,52 @@
+import { registry } from "../../utils/registry";
+import { components } from "../../chart-components/index";
+import { data } from "../../data/index";
+import {
+	builder as buildScales,
+	getOrCreateScale
+} from "./scales";
+
+const regComps = registry();
+regComps.register( "components", components );
+
+const regScales = registry();
+regScales.register( "scales", buildScales );
+
+export function composer () {
+
+	let scales = {},
+		tables = [],
+		comps = {},
+		container = null;
+
+	let fn = function() {};
+
+	fn.build = function( element, meta, settings ) {
+		container = element;
+		fn.data( meta, settings );
+	};
+
+	fn.data = function( meta, settings ) {
+		tables = [data( meta )];
+		scales = regScales.build( settings, this ).scales;
+		comps = regComps.build( settings, this );
+	};
+
+	fn.table = function() {
+		return tables[0];
+	};
+
+	fn.container = function() {
+		return container;
+	};
+
+	fn.scale = function( v ) {
+		return getOrCreateScale( v, scales, tables );
+	};
+
+	fn.components = function() {
+		return comps;
+	};
+
+	return fn;
+}
---
+++ src/core/charts/composer/scales/ordinal.js
@@ -0,0 +1,26 @@
+import { band } from "../../../scales/band";
+
+function unique( values ) {
+	let exists = {};
+	return values.filter( v => {
+		return exists[v.id] ? false : ( exists[v.id] = true );
+	} );
+}
+
+export function ordinal( fields ) {
+	let s = band();
+	let values = fields[0].values();
+	let uniq = unique( values ).map( v => v.label );
+	s.domain( uniq );
+	s.range( [0, 1] );
+	s.paddingOuter( 1 ); // TODO hard-coded
+	s.paddingInner( 1 ); // TODO hard-coded
+	s.align( 0.5 ); // TODO hard-coded
+	let fn = function( v ) {
+		return s.get( v.label );
+	};
+
+	fn.scale = s;
+
+	return fn;
+}
