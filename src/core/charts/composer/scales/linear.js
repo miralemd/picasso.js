b5a11f413d4835296bcee9b96d311f2966c50615
Christofer Bengtsson <cbt@qlik.com>
2016-11-15 10:28:04 +0100

    Add support for expand, include, min and max setting (#69)
    * Add support for expand, include, min and max setting
    
    * Add handling of zero data range
    
    * Add tests for data scale
    
    * Lint fix

--- src/core/charts/composer/scales/linear.js
+++ src/core/charts/composer/scales/linear.js
@@ -1,20 +1,63 @@
 import { linear as lin } from '../../../scales/linear';
 
-function getMinMax(fields) {
+const AVAILABLE_SETTINGS = ['min', 'max', 'expand', 'include', 'invert'];
+
+function evalSetting(fields, settings, name) {
+  if (typeof settings[name] === 'function') {
+    return settings[name](fields);
+  }
+
+  return settings[name];
+}
+
+function generateSettings(fields, settings) {
+  const calcSettings = {};
+  AVAILABLE_SETTINGS.forEach((s) => {
+    calcSettings[s] = evalSetting(fields, settings, s);
+  });
+  return calcSettings;
+}
+
+function getMinMax(fields, settings) {
+  const min = Number(settings.min);
+  const max = Number(settings.max);
+  let fieldMin = Math.min(...fields.map(m => m.min()));
+  let fieldMax = Math.max(...fields.map(m => m.max()));
+
+  if (isNaN(fieldMin) || isNaN(fieldMax)) {
+    fieldMin = -1;
+    fieldMax = 1;
+  } else if (fieldMin === fieldMax && fieldMin === 0) {
+    fieldMin = -1;
+    fieldMax = 1;
+  } else if (fieldMin === fieldMax && fieldMin) {
+    fieldMin -= Math.abs(fieldMin * 0.1);
+    fieldMax += Math.abs(fieldMax * 0.1);
+  } else if (!isNaN(settings.expand)) {
+    const range = fieldMax - fieldMin;
+    fieldMin -= range * settings.expand;
+    fieldMax += range * settings.expand;
+  }
+
+  if (Array.isArray(settings.include)) {
+    const i = settings.include.filter(n => !isNaN(n));
+    fieldMin = Math.min(...i, fieldMin);
+    fieldMax = Math.max(...i, fieldMax);
+  }
+
   return {
-    min: Math.min(...fields.map(m => m.min())),
-    max: Math.max(...fields.map(m => m.max()))
+    min: !isNaN(min) ? min : fieldMin,
+    max: !isNaN(max) ? max : fieldMax
   };
 }
 
 export function linear(fields, settings) {
   const s = lin();
-  let { min, max } = getMinMax(fields);
+  const stgns = generateSettings(fields, settings);
+  const { min, max } = getMinMax(fields, stgns);
   s.domain([min, max]);
-  s.range(settings.invert ? [1, 0] : [0, 1]);
-  const fn = function (v) {
-    return s.get(v.value);
-  };
+  s.range(stgns.invert ? [1, 0] : [0, 1]);
+  const fn = v => s.get(v.value);
 
   fn.scale = s;
 
