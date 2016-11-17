ef9ba2b02782d3ab60f88014c1bec56a1df93dba
Tobias Linsefors <tobias.linsefors@qlik.com>
2016-11-17 10:57:22 +0100

    fix size estimation of tilted labels

--- src/core/chart-components/axis/axis-size-calculator.js
+++ src/core/chart-components/axis/axis-size-calculator.js
@@ -64,8 +64,8 @@ export default function calcRequiredSize({ type, data, formatter, renderer, scal
       let sizeFromTextRect;
       if (tilted) {
         // const radians = Math.PI / 3; // angle in radians
-        const radians = -60 * (Math.PI / 180); // angle in radians
-        sizeFromTextRect = r => (r.height * Math.sin(radians)) + (r.width * Math.cos(radians));
+        const radians = 60 * (Math.PI / 180); // angle in radians
+        sizeFromTextRect = r => (r.width * Math.sin(radians)) + (r.height * Math.cos(radians));
       } else if (horizontal) {
         sizeFromTextRect = r => r.height;
       } else {
