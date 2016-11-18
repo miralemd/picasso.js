d3bd70511286bbd7555b32047e81b5b3f4adf0c2
Tobias Åström <tsm@qlik.com>
2016-11-18 10:38:00 +0100

    Update tests and fix lint

--- src/core/chart-components/axis/axis-default-settings.js
+++ src/core/chart-components/axis/axis-default-settings.js
@@ -1,63 +1,3 @@
-import { default as extend } from 'extend';
-
-const DISCRETE_DEFAULT_STYLE = {
-  labels: {
-    show: true,
-    tilted: false,
-    fontFamily: 'Arial',
-    fontSize: '13px',
-    fill: '#999',
-    padding: 6,
-    layered: false // TODO support auto, true and false?
-  },
-  line: {
-    show: true,
-    strokeWidth: 1,
-    stroke: '#999'
-  },
-  ticks: {
-    show: true,
-    padding: 0,
-    tickSize: 4,
-    stroke: '#999',
-    strokeWidth: 1
-  }
-};
-
-const CONTINOUS_DEFAULT_STYLE = {
-  labels: {
-    show: true,
-    tilted: false,
-    fontFamily: 'Arial',
-    fontSize: '13px',
-    fill: '#999',
-    padding: 4,
-    layered: false
-  },
-  line: {
-    show: true,
-    strokeWidth: 1,
-    stroke: '#999'
-  },
-  ticks: {
-    show: true,
-    padding: 0,
-    tickSize: 8,
-    tight: false,
-    forceBounds: false,
-    stroke: '#999',
-    strokeWidth: 1
-  },
-  minorTicks: {
-    show: false,
-    padding: 0,
-    tickSize: 3,
-    stroke: '#999',
-    strokeWidth: 1,
-    count: 3
-  }
-};
-
 export function discreteDefaultSettings() {
   return [
     {
@@ -65,7 +5,29 @@ export function discreteDefaultSettings() {
       displayOrder: 0,
       prioOrder: 0
     },
-    extend(true, {}, DISCRETE_DEFAULT_STYLE)
+    {
+      labels: {
+        show: true,
+        tilted: false,
+        fontFamily: 'Arial',
+        fontSize: '13px',
+        fill: '#999',
+        padding: 6,
+        layered: false // TODO support auto, true and false?
+      },
+      line: {
+        show: true,
+        strokeWidth: 1,
+        stroke: '#999'
+      },
+      ticks: {
+        show: true,
+        padding: 0,
+        tickSize: 4,
+        stroke: '#999',
+        strokeWidth: 1
+      }
+    }
   ];
 }
 
@@ -76,6 +38,38 @@ export function continuousDefaultSettings() {
       displayOrder: 0,
       prioOrder: 0
     },
-    extend(true, {}, CONTINOUS_DEFAULT_STYLE)
+    {
+      labels: {
+        show: true,
+        tilted: false,
+        fontFamily: 'Arial',
+        fontSize: '13px',
+        fill: '#999',
+        padding: 4,
+        layered: false
+      },
+      line: {
+        show: true,
+        strokeWidth: 1,
+        stroke: '#999'
+      },
+      ticks: {
+        show: true,
+        padding: 0,
+        tickSize: 8,
+        tight: false,
+        forceBounds: false,
+        stroke: '#999',
+        strokeWidth: 1
+      },
+      minorTicks: {
+        show: false,
+        padding: 0,
+        tickSize: 3,
+        stroke: '#999',
+        strokeWidth: 1,
+        count: 3
+      }
+    }
   ];
 }
