0b969e3bfa9dff8619a1b7d4c8cf69f0f93ef61f
Tobias Linsefors <tobias.linsefors@qlik.com>
2016-11-30 13:46:36 +0100

    expose registerComponent

--- src/core/chart-components/index.js
+++ src/core/chart-components/index.js
@@ -11,6 +11,9 @@ reg.register('grid', gridFactory);
 reg.register('axes', axisFactory);
 reg.register('texts', textFactory);
 
+export function register(name, component) {
+  reg.register(name, component);
+}
 
 export default function components(obj, composer) {
   return reg.build(obj, composer);
