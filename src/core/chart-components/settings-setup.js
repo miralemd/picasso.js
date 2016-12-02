a59ef2171ad9502aacc6a835ef311dcb2b9d3f06
Tobias Åström <tsm@qlik.com>
2016-12-02 09:23:43 +0100

    Allow error (null) marker to use custom fn

--- src/core/chart-components/settings-setup.js
+++ src/core/chart-components/settings-setup.js
@@ -1,27 +1,24 @@
 import extend from 'extend';
 import { resolveStyle } from '../style';
 
-export default function resolveInitialSettings(settings, defaults, composer, rootPath) {
-  const resolvers = {};
+export default function resolveSettingsForPath(settings, defaults, composer, path) {
   const composition = extend(true, {}, settings);
-  Object.keys(defaults).forEach((s) => {
-    const ext = rootPath && composition[rootPath] ? composition[rootPath][s] : composition[s];
-    if (ext) {
-      // Check each setting for the existance of a source without custom function
-      Object.keys(ext).forEach((a) => {
-        if (ext[a]) {
-          if ((ext[a].source || ext[a].scale) && !ext[a].fn) {
-            const scale = composer.scale(ext[a]);
-            if (scale) {
-              ext[a] = scale;
-            }
-          } else if (ext[a].fn) {
-            ext[a] = ext[a].fn;
+  // Check each setting for the existance of a source without custom function
+  const ext = path ? composition[path] : composition;
+  const defs = path ? defaults[path] : defaults;
+  if (ext) {
+    Object.keys(ext).forEach((a) => {
+      if (ext[a]) {
+        if ((ext[a].source || ext[a].scale) && !ext[a].fn) {
+          const scale = composer.scale(ext[a]);
+          if (scale) {
+            ext[a] = scale;
           }
+        } else if (ext[a].fn) {
+          ext[a] = ext[a].fn;
         }
-      });
-    }
-    resolvers[s] = resolveStyle(defaults[s], composition, rootPath ? `${rootPath}.${s}` : `${s}`);
-  });
-  return resolvers;
+      }
+    });
+  }
+  return resolveStyle(defs, composition, path);
 }
