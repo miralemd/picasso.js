a49c204fa06637c30b4e0719691fcc635e23b5a9
Christofer Bengtsson <cbt@qlik.com>
2016-12-02 10:06:28 +0100

    Add new settings for discrete scale(#95)

--- src/core/charts/composer/scales/ordinal.js
+++ src/core/charts/composer/scales/ordinal.js
@@ -1,24 +1,48 @@
 import { band } from '../../../scales/band';
 
+const AVAILABLE_SETTINGS = [
+  'padding',
+  'paddingOuter',
+  'paddingInner',
+  'align',
+  'invert'
+];
+
 function unique(values) {
   const exists = {};
-  return values.filter(v =>
-   exists[v.id] ? false : (exists[v.id] = true)
-);
+  return values.filter(v => exists[v.id] ? false : (exists[v.id] = true));
+}
+
+function evalSetting(fields, settings, name) {
+  if (typeof settings[name] === 'function') {
+    return settings[name](fields);
+  }
+
+  return settings[name];
+}
+
+function generateSettings(fields, settings) {
+  const calcSettings = {};
+  AVAILABLE_SETTINGS.forEach((s) => {
+    calcSettings[s] = evalSetting(fields, settings, s);
+  });
+  return calcSettings;
 }
 
 export default function ordinal(fields, settings) {
   const s = band();
   const values = fields[0].values();
+  const stgns = generateSettings(fields, settings);
   const uniq = unique(values).map(v => v.label);
   s.domain(uniq);
-  s.range(settings.invert ? [1, 0] : [0, 1]);
-  s.paddingOuter(1); // TODO hard-coded
-  s.paddingInner(1); // TODO hard-coded
-  s.align(0.5); // TODO hard-coded
-  const fn = function (v) {
-    return s.get(v.label);
-  };
+  s.range(stgns.invert ? [1, 0] : [0, 1]);
+
+  s.padding(isNaN(stgns.padding) ? 1 : stgns.padding);
+  if (!isNaN(stgns.paddingInner)) s.paddingInner(stgns.paddingInner);
+  if (!isNaN(stgns.paddingOuter)) s.paddingOuter(stgns.paddingOuter);
+  s.align(isNaN(stgns.align) ? 0.5 : stgns.align);
+
+  const fn = v => s.get(v.label);
 
   fn.scale = s;
 
