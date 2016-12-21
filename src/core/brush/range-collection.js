0e8b232e778595ca1730008b2b2f001fc85a3ee8
Miralem Drek <mek@qlik.com>
2016-12-21 12:13:42 +0100

    Brush model for ranges

---
+++ src/core/brush/range-collection.js
@@ -0,0 +1,78 @@
+function index(boundaries, point, after) {
+  let i = 0;
+  while (i < boundaries.length && point > boundaries[i]) {
+    ++i;
+  }
+  if (boundaries[i] === point && after) {
+    ++i;
+  }
+  return i;
+}
+
+function contains(boundaries, point) {
+  const len = boundaries.length;
+
+  for (let i = 1; i < len; i += 2) {
+    if (boundaries[i - 1] <= point && point <= boundaries[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export default function rangeCollection() {
+  let boundaries = [];
+
+  function fn() {}
+
+  fn.add = (range) => {
+    const min = range.min;
+    const max = range.max;
+
+    const i0 = index(boundaries, min);
+    const i1 = index(boundaries, max, true);
+
+    let args = [i0, i1 - i0];
+    if (i0 % 2 === 0) {
+      args.push(min);
+    }
+    if (i1 % 2 === 0) {
+      args.push(max);
+    }
+    boundaries.splice(...args);
+  };
+
+  fn.remove = (range) => {
+    const min = range.min;
+    const max = range.max;
+
+    const i0 = index(boundaries, min);
+    const i1 = index(boundaries, max, true);
+
+    let args = [i0, i1 - i0];
+    if (i0 % 2 === 1) {
+      args.push(min);
+    }
+    if (i1 % 2 === 1) {
+      args.push(max);
+    }
+    boundaries.splice(...args);
+  };
+
+  fn.clear = () => (boundaries = []);
+
+  fn.containsValue = value => contains(boundaries, value);
+
+  fn.ranges = () => {
+    let collection = [];
+    for (let i = 1; i < boundaries.length; i += 2) {
+      collection.push({
+        min: boundaries[i - 1],
+        max: boundaries[i]
+      });
+    }
+    return collection;
+  };
+
+  return fn;
+}
