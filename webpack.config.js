d06e35c978f1eb4fbfb090c0ebf305bf7bcb8835
Rickard Svensson <rsv@qlik.com>
2016-12-28 16:52:21 +0100

    Lint more files

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-"use strict"; // eslint-disable-line
+'use strict'; // eslint-disable-line
 /* eslint-env node*/
 
-const path = require( "path" );
-const srcDir = path.resolve( __dirname, "src" );
+const path = require('path');
+
+const srcDir = path.resolve(__dirname, 'src');
 
 module.exports = {
-	entry: {
-		"picasso": path.resolve( srcDir, "index" )
-	},
-	output: {
-		path: path.resolve( __dirname, "dist" ),
-		filename: "[name].js",
-		library: "[name]",
-		libraryTarget: "umd"
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loader: "babel-loader",
-				query: {
-					presets: ["es2015"] // need to have this here instead of in .babelrc until after-work bumps babel dependecy to ^6.0.0
-				},
-				exclude: /node_modules/
-			}
-		]
-	}
+  entry: {
+    picasso: path.resolve(srcDir, 'index')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: '[name]',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'] // need to have this here instead of in .babelrc until after-work bumps babel dependecy to ^6.0.0
+      },
+      exclude: /node_modules/
+    }]
+  }
 };
