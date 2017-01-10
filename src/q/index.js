5e86c7ca989d4fb4d34e671c8480b161bd905191
Miralem Drek <mek@qlik.com>
2017-01-10 17:09:27 +0100

    Add brush helpers for Sense

--- src/q/index.js
+++ src/q/index.js
@@ -4,5 +4,13 @@ import qDataset from './data/q-dataset';
 import { register as registerFormatter } from '../core/formatter';
 import qFormatter from './formatter';
 
+import qBrush from './brush/q-brush';
+
 registerData('q', qDataset);
 registerFormatter('q', qFormatter);
+
+const q = {
+  brush: qBrush
+};
+
+export default q;
