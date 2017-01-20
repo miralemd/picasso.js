bc139e3aa7c0b375b671348e5f9c755ab46e4824
Miralem Drek <mek@qlik.com>
2017-01-20 16:47:06 +0100

    Add basic support for hypercube in stack mode

--- test/unit/q/data/q-field.spec.js
+++ test/unit/q/data/q-field.spec.js
@@ -146,4 +146,284 @@ describe('QField', () => {
       ]);
     });
   });
+
+  describe('with stacked data', () => {
+    describe('containing two dimensions, one measure', () => {
+      const stackedPageWithoutPseudo = {
+        qArea: {
+          qLeft: 0,
+          qTop: 0,
+          qWidth: 2,
+          qHeight: 3
+        },
+        qData: [
+          {
+            qType: 'R',
+            qElemNo: 0,
+            qSubNodes: [
+              {
+                qText: 'Alpha',
+                qElemNo: 1,
+                qValue: 'NaN',
+                qSubNodes: [
+                  { qText: '$666', qElemNo: -1, qValue: 666, qType: 'T' },
+                  { qText: 'a1', qElemNo: 0, qValue: 123, qSubNodes: [{ qValue: 45, qElemNo: 0, qText: '$45.00' }] },
+                  { qText: 'a2', qElemNo: 3, qValue: 135, qSubNodes: [{ qValue: 32, qElemNo: 0, qText: '$32.00' }] }
+                ] },
+              {
+                qText: 'Beta',
+                qElemNo: 3,
+                qValue: 2,
+                qSubNodes: [
+                  { qText: '$666', qElemNo: -1, qValue: 666, qType: 'T' },
+                  { qText: 'b1', qElemNo: 7, qValue: 345, qSubNodes: [{ qValue: 13, qElemNo: 0, qText: '$13.00' }] },
+                  { qText: 'b3', qElemNo: 9, qValue: 276, qSubNodes: [{ qValue: 17, qElemNo: 0, qText: '$17.00' }] }
+                ]
+              }
+            ]
+          }
+        ]
+      };
+
+      it('should extract values from first dimension', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithoutPseudo],
+          idx: 0
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { value: 'NaN', label: 'Alpha', id: 1 },
+          { value: 'NaN', label: 'Alpha', id: 1 },
+          { value: 2, label: 'Beta', id: 3 },
+          { value: 2, label: 'Beta', id: 3 }
+        ]);
+      });
+
+      it('should extract values from second dimension', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithoutPseudo],
+          idx: 1
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { label: 'a1', id: 0, value: 123 },
+          { label: 'a2', id: 3, value: 135 },
+          { label: 'b1', id: 7, value: 345 },
+          { label: 'b3', id: 9, value: 276 }
+        ]);
+      });
+
+      it('should extract values from measure', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithoutPseudo],
+          idx: 2
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { label: '$45.00', id: 0, value: 45 },
+          { label: '$32.00', id: 0, value: 32 },
+          { label: '$13.00', id: 0, value: 13 },
+          { label: '$17.00', id: 0, value: 17 }
+        ]);
+      });
+    });
+
+    describe('containing two dimensions, two measures (pseudo dim)', () => {
+      const stackedPageWithPseudo = {
+        qArea: {
+          qLeft: 0,
+          qTop: 0,
+          qWidth: 2,
+          qHeight: 3
+        },
+        qData: [
+          {
+            qType: 'R',
+            qElemNo: 0,
+            qSubNodes: [
+              {
+                qText: 'Alpha',
+                qElemNo: 1,
+                qValue: 'NaN',
+                qSubNodes: [
+                  {
+                    qText: 'Margin',
+                    qElemNo: 0,
+                    qValue: 'NaN',
+                    qType: 'P',
+                    qSubNodes: [
+                      { qText: 'a1', qValue: 'NaN', qElemNo: 3, qSubNodes: [{ qValue: 0.50, qElemNo: 0, qText: '50%' }] },
+                      { qText: 'a2', qValue: 2014, qElemNo: 4, qSubNodes: [{ qValue: 0.51, qElemNo: 0, qText: '51%' }] },
+                      { qText: 'a3', qValue: 'NaN', qElemNo: 5, qSubNodes: [{ qValue: 0.52, qElemNo: 0, qText: '52%' }] }
+                    ]
+                  },
+                  {
+                    qText: 'Sales',
+                    qElemNo: 1,
+                    qValue: 'NaN',
+                    qType: 'P',
+                    qSubNodes: [
+                      { qText: 'a1', qValue: 'NaN', qElemNo: 3, qSubNodes: [{ qValue: 41, qElemNo: 0, qText: '$41' }] },
+                      { qText: 'a2', qValue: 'NaN', qElemNo: 4, qSubNodes: [{ qValue: 42, qElemNo: 0, qText: '$42' }] },
+                      { qText: 'a3', qValue: 'NaN', qElemNo: 5, qSubNodes: [{ qValue: 43, qElemNo: 0, qText: '$43' }] }
+                    ]
+                  }
+                ]
+              },
+              {
+                qText: 'Beta',
+                qElemNo: 3,
+                qValue: 2,
+                qSubNodes: [
+                  {
+                    qText: 'Margin',
+                    qElemNo: 0,
+                    qValue: 'NaN',
+                    qType: 'P',
+                    qSubNodes: [
+                      { qText: 'b1', qValue: 'NaN', qElemNo: 7, qSubNodes: [{ qValue: 0.60, qElemNo: 0, qText: '60%' }] },
+                      { qText: 'b2', qValue: 'NaN', qElemNo: 8, qSubNodes: [{ qValue: 0.62, qElemNo: 0, qText: '62%' }] }
+                    ]
+                  },
+                  {
+                    qText: 'Sales',
+                    qElemNo: 1,
+                    qValue: 'NaN',
+                    qType: 'P',
+                    qSubNodes: [
+                      { qText: 'b1', qValue: 'NaN', qElemNo: 7, qSubNodes: [{ qValue: 71, qElemNo: 0, qText: '$71' }] },
+                      { qText: 'b2', qValue: 'NaN', qElemNo: 8, qSubNodes: [{ qValue: 72, qElemNo: 0, qText: '$72' }] }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      };
+
+      it('should extract values from first dimension', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithPseudo],
+          idx: 0
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { label: 'Alpha', id: 1, value: 'NaN' },
+          { label: 'Alpha', id: 1, value: 'NaN' },
+          { label: 'Alpha', id: 1, value: 'NaN' },
+          { label: 'Beta', id: 3, value: 2 },
+          { label: 'Beta', id: 3, value: 2 }
+        ]);
+      });
+
+      it('should extract values from second dimension', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithPseudo],
+          idx: 1
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { label: 'a1', id: 3, value: 'NaN' },
+          { label: 'a2', id: 4, value: 2014 },
+          { label: 'a3', id: 5, value: 'NaN' },
+          { label: 'b1', id: 7, value: 'NaN' },
+          { label: 'b2', id: 8, value: 'NaN' }
+        ]);
+      });
+
+      it('should extract values from first measure', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithPseudo],
+          idx: 2
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { label: '50%', id: 0, value: 0.50 },
+          { label: '51%', id: 0, value: 0.51 },
+          { label: '52%', id: 0, value: 0.52 },
+          { label: '60%', id: 0, value: 0.60 },
+          { label: '62%', id: 0, value: 0.62 }
+        ]);
+      });
+
+      it('should extract values from second measure', () => {
+        const dd = {
+          meta: {
+            qMin: 1,
+            qMax: 2,
+            qTags: ['a', 'b'],
+            qStateCounts: {},
+            qFallbackTitle: 'wohoo'
+          },
+          pages: [stackedPageWithPseudo],
+          idx: 3
+        };
+
+        let ff = qField()(dd);
+        let values = ff.values();
+        expect(values).to.deep.equal([
+          { label: '$41', id: 0, value: 41 },
+          { label: '$42', id: 0, value: 42 },
+          { label: '$43', id: 0, value: 43 },
+          { label: '$71', id: 0, value: 71 },
+          { label: '$72', id: 0, value: 72 }
+        ]);
+      });
+    });
+  });
 });
