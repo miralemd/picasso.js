8759d28b6de9cc8b1eb725acd8c429c9fed3a136
Rickard Svensson <rsv@qlik.com>
2017-03-03 15:11:34 +0100

    Fixes for older node versions

--- tools/generate-html-docs.sh
+++ tools/generate-html-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cd ../docs && yarn && npm -s run docs && cd ../tools
-cd generate-html-docs && yarn && node index.js
+cd generate-html-docs && yarn && npm -s run generate
 echo "Open the following path in your browser:"
 echo "file://$(pwd -P)/dist/index.html"
