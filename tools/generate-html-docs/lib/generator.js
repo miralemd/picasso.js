7ad236ad374103fdf6f847cc41936f936cd60ffe
Hannes Lindquist <hannes.lindquist@qlik.com>
2017-03-06 12:37:52 +0100

    Path POSIX did not seem to do what I wanted

--- tools/generate-html-docs/lib/generator.js
+++ tools/generate-html-docs/lib/generator.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = require('path').posix;
+const path = require('path');
 
 const Handlebars = require('handlebars');
 const marked = require('marked');
--- tools/generate-html-docs/lib/index.js
+++ tools/generate-html-docs/lib/index.js
@@ -1,4 +1,4 @@
-const path = require('path').posix;
+const path = require('path');
 const fs = require('fs');
 
 const generateFile = require('./generator').generateFile;
