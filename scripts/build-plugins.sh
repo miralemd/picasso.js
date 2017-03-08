92634fe1c03e67186dd6537a26bd5eeb6dcfb59b
Hannes Lindquist <carlhannes@users.noreply.github.com>
2017-03-08 07:47:37 +0100

    Build plugins shell script (#267)

---
+++ scripts/build-plugins.sh
@@ -0,0 +1,13 @@
+cwd=pwd
+for dir in ./../plugins/*/
+do
+    dir=${dir%*/}
+    dir=${dir##*/}
+
+    echo Building ${dir}
+    cd ../plugins/${dir}
+    yarn
+    npm -s run build
+    npm -s run build:debug
+    cd ${cwd}
+done
