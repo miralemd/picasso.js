88b416d8d4551c45551aaa3e8ebec85f24309000
Rickard Svensson <rsv@qlik.com>
2017-03-14 08:22:33 +0100

    Use exports for q plugin

--- plugins/q/src/index.js
+++ plugins/q/src/index.js
@@ -1,12 +1,14 @@
 import init, { qDataset } from './q';
 import { numberFormat as qNumberFormatter, timeFormat as qTimeFormatter } from './formatter';
-import qBrush from './brush/q-brush';
+import qBrushHelper from './brush/q-brush';
 
-module.exports = function initialize(picasso) {
+export {
+  qBrushHelper
+};
+
+export default function initialize(picasso) {
   init(picasso);
   picasso.data('q', qDataset);
   picasso.formatter('q-number', qNumberFormatter);
   picasso.formatter('q-time', qTimeFormatter);
-};
-
-module.exports.qBrushHelper = qBrush;
+}
