4e29e31493d05ad8a8bcd1aa507ffa08e2478139
Rickard Svensson <rsv@qlik.com>
2017-03-16 16:09:37 +0100

    Fix q plugin for AMD usage

--- plugins/q/src/index.js
+++ plugins/q/src/index.js
@@ -2,13 +2,11 @@ import init, { qDataset } from './q';
 import { numberFormat as qNumberFormatter, timeFormat as qTimeFormatter } from './formatter';
 import qBrushHelper from './brush/q-brush';
 
-export {
-  qBrushHelper
-};
-
-export default function initialize(picasso) {
+module.exports = function initialize(picasso) {
   init(picasso);
   picasso.data('q', qDataset);
   picasso.formatter('q-number', qNumberFormatter);
   picasso.formatter('q-time', qTimeFormatter);
-}
+};
+
+module.exports.qBrushHelper = qBrushHelper;
