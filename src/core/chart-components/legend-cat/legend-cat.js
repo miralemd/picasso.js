4fc25a438724cbb6f85b63a5643b698e2d13f02a
Hannes Lindquist <carlhannes@users.noreply.github.com>
2017-08-08 12:16:27 +0200

    categorical color legend refactor to reduce DRY (#426)

--- src/core/chart-components/legend-cat/legend-cat.js
+++ src/core/chart-components/legend-cat/legend-cat.js
@@ -28,77 +28,121 @@ const defaultSettings = {
   }
 };
 
+/**
+ * Process label items from scale & domain to calculate prefSize or render items
+ *
+ * @param  {object} settings - Settings from the component
+ * @param  {Scale} scale - Scale from the component
+ * @param  {boolean} HORIZONTAL - True if the labels are going to be rendered horizontally
+ * @param  {string} ALIGN - Alignment of the labels, 'left' or 'right'
+ * @param  {Renderer} renderer - Current SVG/Canvas renderer for measuring text
+ * @param  {object} rect - Rendering area rect, X, Y, Width and Height
+ * @param  {object} chart - The chart object
+ * @return {object} - returns labels, maxX and maxY for computing renderable area
+ */
+function processLabelItems({ settings, scale, HORIZONTAL, ALIGN, renderer, rect, chart }) {
+  let title;
+  const domain = scale.domain();
+
+  if (settings.title.text) {
+    title = settings.title.text;
+  } else if (scale && scale.sources && scale.sources[0]) {
+    title = chart.field(scale.sources[0]).field.title();
+  }
+
+  const titleMargin = resolveMargin(settings.title.margin);
+
+  const labels = [];
+  let prevContainer = {};
+  let nextXitem = 0;
+  let nextYitem = 0;
+  let maxX = 0;
+  let maxY = 0;
+
+  if (settings.title.show) {
+    // Title
+    prevContainer = labelItem({
+      x: HORIZONTAL ? nextXitem : 0,
+      y: !HORIZONTAL ? nextYitem : 0,
+      maxWidth: rect ? Math.min(rect.width, settings.title.maxWidthPx) : settings.title.maxWidthPx,
+      color: 'transparent',
+      fill: settings.title.fill,
+      fontSize: settings.title.fontSize,
+      fontFamily: settings.title.fontFamily,
+      labelText: title,
+      renderer,
+      align: ALIGN,
+      renderingArea: rect,
+      margin: titleMargin,
+      symbolPadding: -parseFloat(settings.title.fontSize) // This is too hacky. FIXME
+    });
+
+    labels.push(prevContainer);
+    maxX = prevContainer.x + prevContainer.width;
+    maxY = prevContainer.y + prevContainer.height;
+  }
+
+  // Items
+  domain.forEach((cat, i, all) => {
+    nextXitem += prevContainer.width || 0;
+    nextYitem += prevContainer.height || 0;
+
+    let data = {
+      value: cat,
+      index: i,
+      color: scale(cat)
+    };
+
+    let labelItemDef = resolveForDataObject(settings.item, data, i, all);
+
+    labelItemDef.x = HORIZONTAL ? nextXitem : 0;
+    labelItemDef.y = !HORIZONTAL ? nextYitem : 0;
+    labelItemDef.maxWidth = rect ? Math.min(rect.width, labelItemDef.maxWidthPx) : labelItemDef.maxWidthPx;
+    labelItemDef.color = scale(cat);
+    labelItemDef.labelText = cat;
+    labelItemDef.renderer = renderer;
+    labelItemDef.align = ALIGN;
+    labelItemDef.renderingArea = rect;
+    labelItemDef.margin = resolveMargin(labelItemDef.margin);
+
+    prevContainer = labelItem(labelItemDef);
+
+    labels.push(prevContainer);
+    maxX = Math.max(maxX, prevContainer.x + prevContainer.width);
+    maxY = Math.max(maxY, prevContainer.y + prevContainer.height);
+  });
+
+  return {
+    labels,
+    maxX,
+    maxY
+  };
+}
+
+/**
+ * Categorical Color Legend Component
+ * @type {Object}
+ * @ignore
+ */
 const categoricalLegend = {
   require: ['chart', 'settings', 'renderer'],
   defaultSettings,
   preferredSize() {
     const scale = this.chart.scale(this.settings.scale);
-    const domain = scale.domain();
     const DOCK = this.settings.dock || 'center';
     const DIRECTION = this.settings.direction || ((DOCK === 'top' || DOCK === 'bottom') ? 'horizontal' : 'vertical');
     const HORIZONTAL = (DIRECTION === 'horizontal');
-    let title = '';
 
-    if (this.settings.title.text) {
-      title = this.settings.title.text;
-    } else if (scale && scale.sources && scale.sources[0]) {
-      title = this.chart.field(scale.sources[0]).field.title();
-    }
+    const {
+      settings,
+      renderer,
+      chart
+    } = this;
 
-    const titleMargin = resolveMargin(this.settings.title.margin);
-
-    let prevContainer = {};
-    let nextXitem = 0;
-    let nextYitem = 0;
-    let maxX = 0;
-    let maxY = 0;
-
-    if (this.settings.title.show) {
-      // Title
-      prevContainer = labelItem({
-        x: HORIZONTAL ? nextXitem : 0,
-        y: !HORIZONTAL ? nextYitem : 0,
-        maxWidth: this.settings.title.maxWidthPx,
-        fontSize: this.settings.title.fontSize,
-        fontFamily: this.settings.title.fontFamily,
-        labelText: title,
-        renderer: this.renderer,
-        renderingArea: this.rect,
-        margin: titleMargin,
-        symbolPadding: -parseFloat(this.settings.title.fontSize) // This is too hacky. FIXME
-      });
-
-      maxX = prevContainer.x + prevContainer.width;
-      maxY = prevContainer.y + prevContainer.height;
-    }
-
-    // Items
-    domain.forEach((cat, i, all) => {
-      nextXitem += prevContainer.width || 0;
-      nextYitem += prevContainer.height || 0;
-
-      let data = {
-        value: cat,
-        index: i,
-        color: scale(cat)
-      };
-
-      let labelItemDef = resolveForDataObject(this.settings.item, data, i, all);
-
-      labelItemDef.x = HORIZONTAL ? nextXitem : 0;
-      labelItemDef.y = !HORIZONTAL ? nextYitem : 0;
-      labelItemDef.maxWidth = labelItemDef.maxWidthPx;
-      labelItemDef.color = scale(cat);
-      labelItemDef.labelText = cat;
-      labelItemDef.renderer = this.renderer;
-      labelItemDef.renderingArea = this.rect;
-      labelItemDef.margin = resolveMargin(labelItemDef.margin);
-
-      prevContainer = labelItem(labelItemDef);
-
-      maxX = Math.max(maxX, prevContainer.x + prevContainer.width);
-      maxY = Math.max(maxY, prevContainer.y + prevContainer.height);
-    });
+    const {
+      maxX,
+      maxY
+    } = processLabelItems({ settings, scale, HORIZONTAL, renderer, chart });
 
     return DOCK === 'left' || DOCK === 'right' ? maxX : maxY;
   },
@@ -110,74 +154,21 @@ const categoricalLegend = {
   },
   render() {
     const scale = this.chart.scale(this.settings.scale);
-    const domain = scale.domain();
     const DOCK = this.settings.dock || 'center';
     const ALIGN = this.settings.align;
     const DIRECTION = this.settings.direction || ((DOCK === 'top' || DOCK === 'bottom') ? 'horizontal' : 'vertical');
     const HORIZONTAL = (DIRECTION === 'horizontal');
-    let title = '';
 
-    if (this.settings.title.text) {
-      title = this.settings.title.text;
-    } else if (scale && scale.sources && scale.sources[0]) {
-      title = this.chart.field(scale.sources[0]).field.title();
-    }
+    const {
+      settings,
+      renderer,
+      rect,
+      chart
+    } = this;
 
-    const titleMargin = resolveMargin(this.settings.title.margin);
-
-    const labels = [];
-    let prevContainer = {};
-    let nextXitem = 0;
-    let nextYitem = 0;
-
-    if (this.settings.title.show) {
-      // Title
-      prevContainer = labelItem({
-        x: HORIZONTAL ? nextXitem : 0,
-        y: !HORIZONTAL ? nextYitem : 0,
-        maxWidth: Math.min(this.rect.width, this.settings.title.maxWidthPx),
-        color: 'transparent',
-        fill: this.settings.title.fill,
-        fontSize: this.settings.title.fontSize,
-        fontFamily: this.settings.title.fontFamily,
-        labelText: title,
-        renderer: this.renderer,
-        align: ALIGN,
-        renderingArea: this.rect,
-        margin: titleMargin,
-        symbolPadding: -parseFloat(this.settings.title.fontSize) // This is too hacky. FIXME
-      });
-
-      labels.push(prevContainer);
-    }
-
-    // Items
-    domain.forEach((cat, i, all) => {
-      nextXitem += prevContainer.width || 0;
-      nextYitem += prevContainer.height || 0;
-
-      let data = {
-        value: cat,
-        index: i,
-        color: scale(cat)
-      };
-
-      let labelItemDef = resolveForDataObject(this.settings.item, data, i, all);
-
-      labelItemDef.x = HORIZONTAL ? nextXitem : 0;
-      labelItemDef.y = !HORIZONTAL ? nextYitem : 0;
-      labelItemDef.maxWidth = Math.min(this.rect.width, labelItemDef.maxWidthPx);
-      labelItemDef.color = scale(cat);
-      labelItemDef.labelText = cat;
-      labelItemDef.renderer = this.renderer;
-      labelItemDef.align = ALIGN;
-      labelItemDef.renderingArea = this.rect;
-      labelItemDef.margin = resolveMargin(labelItemDef.margin);
-
-      prevContainer = labelItem(labelItemDef);
-
-      labels.push(prevContainer);
-    });
+    const {
+      labels
+    } = processLabelItems({ settings, scale, HORIZONTAL, ALIGN, renderer, rect, chart });
 
     return labels;
   }
