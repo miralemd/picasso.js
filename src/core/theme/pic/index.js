d5970c169345076f911c75f7d32c3acd3aa706a5
Miralem Drek <mek@qlik.com>
2017-09-29 15:23:04 +0200

    Add basic theme

---
+++ src/core/theme/pic/index.js
@@ -0,0 +1,39 @@
+const palettes = [
+  {
+    key: 'categorical',
+    colors: [
+      ['#a54343', '#d76c6c', '#ec983d', '#ecc43d', '#f9ec86', '#cbe989', '#70ba6e', '#578b60', '#79d69f', '#26a0a7', '#138185', '#65d3da'] // breeze colors
+    ]
+  },
+  {
+    key: 'diverging',
+    colors: [
+      ['#3d52a1', '#3a89c9', '#77b7e5', '#b4ddf7', '#e6f5fe', '#ffe3aa', '#f9bd7e', '#ed875e', '#d24d3e', '#ae1c3e']
+    ]
+  },
+  {
+    key: 'sequential',
+    colors: [
+      ['rgb(180,221,212)', 'rgb(34, 83, 90)']
+    ]
+  }
+];
+
+/* eslint quote-props: 0 */
+const style = {
+  '$font-family': 'Arial',
+  '$font-size-m': '12px',
+  '$line-height-m': '16px',
+
+  '$primary': 'steelblue',
+  '$shape': {
+    fill: '$primary',
+    strokeWidth: 0,
+    stroke: '#ccc'
+  }
+};
+
+export {
+  style,
+  palettes
+};
