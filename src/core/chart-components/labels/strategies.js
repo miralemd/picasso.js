69a6f92b971e922a97aece6c8e8011541eed0223
Miralem Drek <mek@qlik.com>
2017-10-03 08:35:38 +0200

    Refactor and test

--- src/core/chart-components/labels/strategies.js
+++ src/core/chart-components/labels/strategies.js
@@ -1,7 +1,7 @@
 import extend from 'extend';
 import collisions from '../../math/narrow-phase-collision';
 
-const LINE_HEIGHT = 1.4;
+const LINE_HEIGHT = 1.5;
 const PADDING = 4;
 // const DOUBLE_PADDING = PADDING * 2;
 
@@ -19,13 +19,13 @@ export function placeTextInRect(rect, text, opts) {
   const label = {
     type: 'text',
     text,
-    maxWidth: rect.width,
+    maxWidth: opts.rotate ? rect.height : rect.width,
     x: 0,
     y: rect.y,
     dx: 0,
     dy: 0,
     fill: opts.fill,
-    anchor: 'start',
+    anchor: opts.rotate ? 'end' : 'start',
     baseline: 'alphabetical',
     fontSize: `${opts.fontSize}px`,
     fontFamily: opts.fontFamily
@@ -37,46 +37,18 @@ export function placeTextInRect(rect, text, opts) {
     return false;
   }
 
-  const wiggleWidth = Math.max(0, rect.width - textMetrics.width);
-  const wiggleHeight = Math.max(0, rect.height - (textMetrics.height * 1.4));
-  label.x = rect.x + (opts.align * wiggleWidth);
-  label.y = rect.y + textMetrics.height + (opts.justify * wiggleHeight);
-
-  if (opts.transform) {
-    label.transform = `rotate(90, ${label.x + label.dx}, ${label.y + label.dy})`;
+  if (opts.rotate) {
+    const wiggleHor = Math.max(0, rect.width - (textMetrics.height / (LINE_HEIGHT * 0.8)));
+    const wiggleVert = Math.max(0, rect.height - textMetrics.width);
+    label.x = rect.x + (textMetrics.height / LINE_HEIGHT) + (opts.align * wiggleHor);
+    label.y = rect.y + (opts.justify * wiggleVert);
+    label.transform = `rotate(-90, ${label.x + label.dx}, ${label.y + label.dy})`;
+  } else {
+    const wiggleWidth = Math.max(0, rect.width - textMetrics.width);
+    const wiggleHeight = Math.max(0, rect.height - (textMetrics.height / (LINE_HEIGHT * 0.8))); // 0.8 - MAGIC NUMBER - need to figure out why this works the best
+    label.x = rect.x + (opts.align * wiggleWidth);
+    label.y = rect.y + (textMetrics.height / LINE_HEIGHT) + (opts.justify * wiggleHeight);
   }
-
-  return label;
-}
-
-export function placeVerticalTextInRect(rect, text, opts) {
-  const label = {
-    type: 'text',
-    text,
-    maxWidth: rect.height,
-    x: 0,
-    y: rect.y,
-    dx: 0,
-    dy: 0,
-    fill: opts.fill,
-    anchor: 'end',
-    baseline: 'alphabetical',
-    fontSize: `${opts.fontSize}px`,
-    fontFamily: opts.fontFamily
-  };
-
-  const textMetrics = opts.textMetrics;
-
-  if (rect.width < opts.fontSize || rect.height < textMetrics.height * LINE_HEIGHT) {
-    return false;
-  }
-
-  const wiggleHor = Math.max(0, rect.width - (textMetrics.height * LINE_HEIGHT));
-  const wiggleVert = Math.max(0, rect.height - textMetrics.width);
-  label.x = rect.x + textMetrics.height + (opts.align * wiggleHor);
-  label.y = rect.y + (opts.justify * wiggleVert);
-
-  label.transform = `rotate(-90, ${label.x + label.dx}, ${label.y + label.dy})`;
 
   return label;
 }
@@ -135,7 +107,59 @@ export function getBarRect({ bar, view, direction, position, padding = PADDING }
   return bounds;
 }
 
-function placeInBars({
+export function findBestPlacement({
+  direction,
+  fitsHorizontally,
+  lblStngs,
+  measured,
+  node,
+  orientation,
+  // placements,
+  placementSettings,
+  rect
+}, barRect = getBarRect) {
+  let largest;
+  let bounds;
+  let placement;
+  let testBounds;
+  let p;
+  const boundaries = [];
+  for (p = 0; p < placementSettings.length; p++) {
+    placement = placementSettings[p];
+    testBounds = barRect({
+      bar: node.localBounds,
+      view: rect,
+      direction,
+      position: placement.position
+    });
+    boundaries.push(testBounds);
+    largest = !p || testBounds.height > largest.height ? testBounds : largest;
+
+    if (orientation === 'v' && ((fitsHorizontally && testBounds.height > measured.height * LINE_HEIGHT) ||
+      (!fitsHorizontally && testBounds.height > measured.width))) {
+      bounds = testBounds;
+      break;
+    } else if (orientation === 'h' && (testBounds.height > measured.height) &&
+      (testBounds.width > measured.width)) {
+      bounds = testBounds;
+      break;
+    }
+  }
+
+  // fallback strategy - place the text in the largest rectangle
+  if (orientation === 'v' && !fitsHorizontally && !bounds && largest.height > lblStngs.fontSize * 2) {
+    bounds = largest;
+    p = boundaries.indexOf(bounds);
+  } else if (orientation === 'h' && !bounds && largest.height > lblStngs.fontSize * 2) {
+    bounds = largest;
+    p = boundaries.indexOf(bounds);
+  }
+  placement = placementSettings[p];
+
+  return { bounds, placement };
+}
+
+export function placeInBars({
   chart,
   nodes,
   // stngs,
@@ -147,86 +171,62 @@ function placeInBars({
   rect,
   fitsHorizontally,
   collectiveOrientation
-}) {
+}, findPlacement = findBestPlacement,
+  placer = placeTextInRect) {
   const labels = [];
-  const textPlacementFn = collectiveOrientation === 'h' || fitsHorizontally ? placeTextInRect : placeVerticalTextInRect;
+  // const textPlacementFn = collectiveOrientation === 'h' || fitsHorizontally ? placeTextInRect : placeTextInRect;
   let label;
   let node;
   let text;
   let justify;
   let bounds;
   let fill;
-  let testBounds;
+  // let testBounds;
   let measured;
   let direction;
   let lblStngs;
   let nodeTexts;
   let placement;
   let placements;
-  let p;
+  // let p;
   let arg;
   let orientation;
 
   for (let i = 0, len = nodes.length; i < len; i++) {
     bounds = null;
     node = nodes[i];
-    // d = node.data;
     arg = cbContext(node, chart);
     nodeTexts = texts[i];
-    direction = directions[i]; // typeof stngs.direction === 'function' ? stngs.direction(arg, i) : stngs.direction || 'up';
+    direction = directions[i];
     orientation = direction === 'left' || direction === 'right' ? 'h' : 'v';
     for (let j = 0; j < nodeTexts.length; j++) {
       text = nodeTexts[j];
       if (!text) {
-        continue; // eslint-ignore-line
+        continue;
       }
 
       lblStngs = labelSettings[j];
       measured = measurements[i][j];
       placements = lblStngs.placements;
 
-      let largest;
-      const boundaries = [];
+      const bestPlacement = findPlacement({
+        direction,
+        fitsHorizontally,
+        lblStngs,
+        measured,
+        node,
+        orientation,
+        placements,
+        placementSettings: placementSettings[j],
+        rect
+      });
 
-      for (p = 0; p < placements.length; p++) {
-        placement = placementSettings[j][p];
-        testBounds = getBarRect({
-          bar: node.localBounds,
-          view: rect,
-          direction,
-          position: placement.position
-        });
-        boundaries.push(testBounds);
-        largest = !p || testBounds.height > largest.height ? testBounds : largest;
-
-        if (orientation === 'v' && ((fitsHorizontally && testBounds.height > measured.height * LINE_HEIGHT) ||
-          (!fitsHorizontally && testBounds.height > measured.width))) {
-          bounds = testBounds;
-          break;
-        } else if (orientation === 'h' && (testBounds.height > measured.height) &&
-          (testBounds.width > measured.width)) {
-          bounds = testBounds;
-          break;
-        }
-      }
-
-      // fallback strategy - place the text in the largest rectangle
-      if (orientation === 'v' && !fitsHorizontally && !bounds && largest.height > lblStngs.fontSize * 2) {
-        bounds = largest;
-        p = boundaries.indexOf(bounds);
-      } else if (orientation === 'h' && !bounds && largest.height > lblStngs.fontSize * 2) {
-        bounds = largest;
-        p = boundaries.indexOf(bounds);
-      }
-      placement = placementSettings[j][p];
+      bounds = bestPlacement.bounds;
+      placement = bestPlacement.placement;
 
       if (bounds && placement) {
         justify = placement.justify;
-        if (typeof placement.fill === 'function') {
-          fill = placement.fill(arg, i);
-        } else {
-          fill = placement.fill;
-        }
+        fill = typeof placement.fill === 'function' ? placement.fill(arg, i) : placement.fill;
 
         if (direction === 'up') {
           justify = 1 - justify;
@@ -235,13 +235,14 @@ function placeInBars({
           justify = 1 - justify;
         }
 
-        label = textPlacementFn(bounds, text, {
+        label = placer(bounds, text, {
           fill,
           justify: orientation === 'h' ? placement.align : justify,
           align: orientation === 'h' ? justify : placement.align,
           fontSize: lblStngs.fontSize,
           fontFamily: lblStngs.fontFamily,
-          textMetrics: measured
+          textMetrics: measured,
+          rotate: !(collectiveOrientation === 'h' || fitsHorizontally)
         });
 
         if (label) {
@@ -254,36 +255,14 @@ function placeInBars({
   return labels;
 }
 
-/**
- * @typedef settings
- * @type {object}
- * @property {string|function} [direction='up'] - The direction in which the bars are growing: 'up', or 'down'
- * @property {string} [fontFamily='Arial']
- * @property {number} [fontSize=12]
- * @property {object} labels
- * @property {string|function} labels.label - The text value
- * @property {object} labels.placements
- * @property {string} labels.placements.position,
- * @property {number} [labels.placements.justify=0] - Placement of the label along the direction of the bar
- * @property {number} [labels.placements.align=0.5] - Placement of the label along the perpendicular direction of the bar
- * @property {string} [labels.placements.fill='#333'] - Color of the label
- */
-
-export function bars({
-  settings,
-  chart,
+export function precalculate({
   nodes,
-  data,
   rect,
+  chart,
+  labelSettings,
+  settings,
   renderer
 }) {
-  const defaults = {
-    fontSize: 12,
-    fontFamily: 'Arial',
-    align: 0.5,
-    justify: 0,
-    fill: '#333'
-  };
   const measurements = [];
   const texts = [];
   const labelStruct = {};
@@ -297,25 +276,14 @@ export function bars({
   let lblStng;
   let direction;
 
-  const labelSettings = settings.labels.map(labelSetting =>
-    extend({}, defaults, settings, labelSetting)
-  );
-
-  const placementSettings = settings.labels.map(labelSetting =>
-    labelSetting.placements.map(placement =>
-      extend({}, defaults, settings, labelSetting, placement)
-    )
-  );
-
-  // step one - measure if all labels fit horizontally
   for (let i = 0, len = nodes.length; i < len; i++) {
-    texts[i] = [];
-    measurements[i] = [];
     node = nodes[i];
     bounds = node.localBounds;
     if (!collisions.testRectRect(bounds, rect)) {
       continue;
     }
+    texts[i] = [];
+    measurements[i] = [];
     let arg = cbContext(node, chart);
     for (let j = 0; j < labelSettings.length; j++) {
       lblStng = labelSettings[j];
@@ -338,7 +306,74 @@ export function bars({
     }
   }
 
-  return placeInBars({
+  return {
+    measurements,
+    texts,
+    directions,
+    fitsHorizontally,
+    hasHorizontalDirection
+  };
+}
+
+/**
+ * @typedef settings
+ * @type {object}
+ * @property {string|function} [direction='up'] - The direction in which the bars are growing: 'up', 'down', 'right' or 'left'.
+ * @property {string} [fontFamily='Arial']
+ * @property {number} [fontSize=12]
+ * @property {object} labels
+ * @property {string|function} labels.label - The text value
+ * @property {object} labels.placements
+ * @property {string} labels.placements.position,
+ * @property {number} [labels.placements.justify=0] - Placement of the label along the direction of the bar
+ * @property {number} [labels.placements.align=0.5] - Placement of the label along the perpendicular direction of the bar
+ * @property {string} [labels.placements.fill='#333'] - Color of the label
+ */
+
+export function bars({
+  settings,
+  chart,
+  nodes,
+  rect,
+  renderer,
+  style
+}, placer = placeInBars) {
+  const defaults = extend({
+    fontSize: 12,
+    fontFamily: 'Arial',
+    align: 0.5,
+    justify: 0,
+    fill: '#333'
+  }, style.label);
+
+  defaults.fontSize = parseInt(defaults.fontSize, 10);
+
+  const labelSettings = settings.labels.map(labelSetting =>
+    extend({}, defaults, settings, labelSetting)
+  );
+
+  const placementSettings = settings.labels.map(labelSetting =>
+    labelSetting.placements.map(placement =>
+      extend({}, defaults, settings, labelSetting, placement)
+    )
+  );
+
+  const {
+    texts,
+    measurements,
+    fitsHorizontally,
+    hasHorizontalDirection,
+    directions
+  } = precalculate({
+    nodes,
+    chart,
+    renderer,
+    settings,
+    rect,
+    labelSettings
+  });
+
+  return placer({
     chart,
     nodes,
     texts,
@@ -347,7 +382,6 @@ export function bars({
     stngs: settings,
     placementSettings,
     labelSettings,
-    data,
     rect,
     fitsHorizontally,
     collectiveOrientation: hasHorizontalDirection ? 'h' : 'v'
