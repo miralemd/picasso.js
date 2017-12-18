85891228bd29637ae63162c5b1e35af29a0214a0
Christoffer Åström <stoffeastrom@gmail.com>
2017-12-18 00:59:25 -0800

    test(aw): update config (#607)
    Enable running tests locally under packages/plugins
    and handle coverage, save and rerun etz

--- aw.config.unit.coverage.js
+++ aw.config.unit.coverage.js
@@ -1,6 +1,8 @@
 module.exports = {
   require: ['babel-register', 'babel-helpers'],
   glob: ['*(packages|plugins)/*/test/unit/**/*.spec.js'],
+  watchGlob: ['*(packages|plugins)/*/*(src|test)/**/*.js'],
+  src: ['*(packages|plugins)/*/src/**/*.js'],
   coverage: true,
   nyc: {
     reportDir: 'coverage/unit'
